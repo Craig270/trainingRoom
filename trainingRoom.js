5dfe68082aec38f5ae0ea2a67542bc71ab54067e
Craig Neff <ifly12610@gmail.com>
2021-02-22 00:37:10 -0700

    on to the last problem for chapter 3

--- trainingRoom.js
+++ trainingRoom.js
@@ -1,68 +1,31 @@
 "use strict";
-/*
-//Write a loop that makes seven calls to console.log to output the following triangle:
 
-#
-##
-###
-####
-#####
-######
-#######
-*/
+/*Minimum
+The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum. */
 
-let justin = "#";
-
-while (justin.length < 7) {
-  console.log(justin);
-  justin += "#";
+function findTheMin(num1, num2) {
+  return Math.min(num1, num2);
 }
 
-/*
-FizzBuzz
-Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+console.log(findTheMin(10, 1));
 
-When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+/*Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
 
-(This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.) */
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?*/
 
-for (let i = 0; i < 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log(`FizzBuzz`);
-  } else if (i % 3 == 0) {
-    console.log(`Fizz`);
-  } else if (i % 5 == 0) {
-    console.log(`Buzz`);
-  } else console.log(i);
-}
-
-/*
-Chessboard
-Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
-
-Passing this string to console.log should show something like this:
-
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # #
-*/
-
-let size = 8;
-let board = " ";
-
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
-      board = board + " ";
-    } else {
-      board = board + "#";
-    }
+function isEven(num) {
+  if (num < 0) {
+    num = Math.abs(num);
   }
-  board += "\n";
+  if (num === 0) {
+    return true;
+  }
+  if (num === 1) {
+    return false;
+  }
+  return isEven(num - 2);
 }
-console.log(board);
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(Math.abs(-1));
