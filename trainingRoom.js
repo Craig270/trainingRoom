75b3283f96be301c9611e1d80b172f2849ed4a06
Craig Neff <ifly12610@gmail.com>
2021-02-21 19:56:28 -0700

    working on chaper 2 exercises in eloquentjavascript

--- trainingRoom.js
+++ trainingRoom.js
@@ -1,39 +1,19 @@
-// 4a. New function named processSplicedValue that takes 3 parameters (array, the index of the element to be spliced from the array, and a callback function). Inside function, use the first two parameters to splice an element from array.Inside the function, call the callback function and pass the spliced value into it.Outside of the function, create an array of strings, call processSplicedValue, and pass in the array you just created, an index number, and console.log into it.
+"use strict";
+/*
+//Write a loop that makes seven calls to console.log to output the following triangle:
 
-const heroList = [
-  "Captain America",
-  "Falcon",
-  "Ironman",
-  "Spider-Man",
-  "Hulk",
-  "Thor",
-  "War Machine",
-  "Black Widow",
-  "Black Panther",
-  "Doctor Strange",
-];
+#
+##
+###
+####
+#####
+######
+#######
+*/
 
-console.log(heroList);
+let justin = "#";
 
-function bestHero(me) {
-  console.log(`The best hero is ${me}`);
+while (justin.length < 7) {
+  console.log(justin);
+  justin += "#";
 }
-
-function processSplicedValue(array, spliceInd, functionProcess) {
-  var hero = processSplicedValueList.splice(spliceInd, 1);
-  functionProcess(hero);
-}
-
-console.log(9 + 9);
-// processSplicedValue called with console.log.
-processSplicedValue(processSplicedValueList, 5, console.log);
-
-// processSplicedValue called with an alert.
-processSplicedValue(processSplicedValueList, 4, alert);
-
-// pass in an anonymous arrow function that alerts the spliced value.
-processSplicedValue(processSplicedValueList, 0, (x) => alert(x));
-
-//pass in a custom function of your choice that you should create and name.
-console.log(processSplicedValueList);
-processSplicedValue(processSplicedValueList, 0, bestHero);
