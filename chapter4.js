c90c188484f3214e2c2f21fe334fd85654f735d3
Craig Neff <ifly12610@gmail.com>
2021-02-28 22:51:47 -0700

    started chapter 4 of Eloquent Javascript

--- chapter4.js
+++ chapter4.js
@@ -1,10 +1,22 @@
 "use strict";
-
+/*
 function mrAddUp(start, end) {
   console.log(start);
   while (start < end) {
     console.log((start += 1));
   }
+}
+
+mrAddUp(1, 101);
+*/
+
+function mrAddUp(start, end) {
+  let newArray = [];
+  while (start <= end) {
+    newArray.push(start);
+    start += 1;
+  }
+  console.log(newArray);
 }
 
 mrAddUp(1, 101);
