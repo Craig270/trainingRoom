b2502d0a4d4f0b2d27da575f6801a2935a9796d2
Craig Neff <ifly12610@gmail.com>
2021-03-05 19:59:48 -0700

    Made a deck of cards using classes for both the deck and card, learned how to shuffle then deal those cards! Also added a scoreboard!

--- trainingRoom.js
+++ trainingRoom.js
@@ -85,31 +85,87 @@ startWarDeck1.makeAdeck();
 console.log(startWarDeck1.newDeck);
 startWarDeck1.shuffle(startWarDeck1.newDeck);
 
-////Score Keeper mock up
-// let hotelScore = 0;
-// let myDeckScore = 0;
+for (let i = 0; i < startWarDeck1.newDeck.length; i++) {
+  if (i % 2 == 0) {
+    craig.hand.push(startWarDeck1.newDeck[i]);
+  } else {
+    jennifer.hand.push(startWarDeck1.newDeck[i]);
+  }
+}
+console.log(craig.hand);
+console.log(jennifer.hand);
+console.log(craig.hand[0].value);
+console.log(jennifer.hand[0].value);
+console.log(craig.hand[0].suit);
 
-// console.log(`The Score:
-// --------------
-// Hotel Deck:${hotelScore}
-// myDeck:${myDeckScore}
-// `);
-// if (hotelDeck.newDeck[1].value > myDeck.newDeck[1].value) {
-//   hotelScore += 1;
-//   console.log(`Congrats! Hotel Deck Won!`);
-// } else if (hotelDeck.newDeck[1].value < myDeck.newDeck[1].value) {
-//   myDeckScore += 1;
-//   console.log(`Congrats! myDeck Deck Won!`);
-// } else if (hotelDeck.newDeck[1].value == myDeck.newDeck[1].value) {
-//   console.log(`This battle of war was a tie! No player got a point!`);
-// }
+let player1Score = 0;
+let player2Score = 0;
 
-// console.log(`The Score:
-// --------------
-// Hotel Deck:${hotelScore}
-// myDeck:${myDeckScore}
-// `);
+for (let i = 0; i < craig.hand.length; i++) {
+  if (craig.hand[i].value > jennifer.hand[i].value) {
+    player1Score += 1;
+    console.log(
+      `${craig.name} played ${craig.hand[i].rank} of ${craig.hand[i].suit} beating out ${jennifer.hand[i].rank} of ${jennifer.hand[i].suit} played by ${jennifer.name}!`
+    );
+  } else if (craig.hand[i].value < jennifer.hand[i].value) {
+    player2Score += 1;
+    console.log(
+      `${jennifer.name} played ${jennifer.hand[i].rank} of ${jennifer.hand[i].suit} beating out ${craig.hand[i].rank} of ${craig.hand[i].suit} played by ${craig.name}`
+    );
+  } else if (craig.hand[i].value == jennifer.hand[i].value) {
+    console.log(`This battle of war was a tie! No player got a point!`);
+  }
+}
+console.log(`
+--------------
+The Score:
+--------------
+${craig.name}:${player1Score}
+${jennifer.name}:${player2Score}
+`);
+if (player1Score > player2Score) {
+  console.log(
+    `Congratulations!! ${craig.name} you are the winner of this War Card Game simuilated game!!`
+  );
+}
+if (player2Score > player1Score) {
+  console.log(
+    `Congratulations!! ${jennifer.name} you are the winner of this War Card Game simuilated game!!`
+  );
+} else if (player1Score === player1Score) {
+  console.log(
+    `As luck would have it this game was a tie and no one is the winner!`
+  );
+}
 
+/*
+Score Keeper mock up
+let hotelScore = 0;
+let myDeckScore = 0;
+
+console.log(`The Score:
+--------------
+Hotel Deck:${hotelScore}
+myDeck:${myDeckScore}
+`);
+if (hotelDeck.newDeck[1].value > myDeck.newDeck[1].value) {
+  hotelScore += 1;
+  console.log(`Congrats! Hotel Deck Won!`);
+} else if (hotelDeck.newDeck[1].value < myDeck.newDeck[1].value) {
+  myDeckScore += 1;
+  console.log(`Congrats! myDeck Deck Won!`);
+} else if (hotelDeck.newDeck[1].value == myDeck.newDeck[1].value) {
+  console.log(`This battle of war was a tie! No player got a point!`);
+}
+
+console.log(`The Score:
+--------------
+Hotel Deck:${hotelScore}
+myDeck:${myDeckScore}
+`);
+
+
+*/
 function startWar() {
   let startWarDeck = new Deck();
   startWarDeck.makeAdeck();
