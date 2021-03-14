64b963e362e9bf1562c02499ae8410387783d061
Craig Neff <ifly12610@gmail.com>
2021-03-13 21:59:35 -0700

    added the simple webpage I made messing around with html

--- trainingRoom.js
+++ trainingRoom.js
@@ -1,3 +1,18 @@
 "use strict";
 
-//Starting a fresh week and sheet
+class Particle {
+  constructor() {
+    this.x = 100;
+    this.y = 99;
+  }
+  show() {
+    point(this.x, this.y);
+  }
+}
+
+var p;
+
+function setup() {
+  createCanvas(600, 300);
+  p = new Particle();
+}
