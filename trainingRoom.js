b02bbcd15755944aa46b535237bfc5b782c31d06
Craig Neff <ifly12610@gmail.com>
2021-03-18 10:18:00 -0600

    practicing CSS in-line layouts

--- trainingRoom.js
+++ trainingRoom.js
@@ -1,18 +1,5 @@
-"use strict";
-
-class Particle {
-  constructor() {
-    this.x = 100;
-    this.y = 99;
-  }
-  show() {
-    point(this.x, this.y);
-  }
-}
-
-var p;
-
-function setup() {
-  createCanvas(600, 300);
-  p = new Particle();
+function divisible(num) {
+  if (num % 100 == 0) {
+    return true;
+  } else return false;
 }
